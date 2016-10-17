45260c3437b507d0983d8361bab1e3980826fa60
Forrest Blanton <fblanton@gmail.com>
2016-10-17 15:26:58 -0700

    Added env variables.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const MongoClient = require('mongodb');
 const createApp = require('./create-app');
 
-const PORT = 1337;
-const URI = 'mongodb://localhost:27017/books'
+const PORT = process.env.PORT || 1337;
+const DEFAULT_URI = 'mongodb://localhost:27017/books'
+const MONGO_URI = process.env.MONGODB_URI || DEFAULT_URI
 
 MongoClient.connect(URI, (err, db) => {
   if (err) {
